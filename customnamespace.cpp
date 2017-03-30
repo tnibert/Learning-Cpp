03d09d3e33f8872b9b408eb5ad22c89148d5d93d
Tim <docz2a@gmail.com>
2017-03-30 18:01:13 +1100

    more namespace stuff

--- customnamespace.cpp
+++ customnamespace.cpp
@@ -21,17 +21,31 @@ namespace bar
 
 int main()
 {
+	namespace spam = foo;	//aliasing
 	cout << foo::value() << endl;
 	cout << bar::value() << endl;
 	cout << bar::pi << endl;
 	bar::a *= 2;
 	cout << bar::value() << endl;
 
-	using foo::a;
+	//using foo::a;
 	using bar::b;
 	//now a and b are from foo and bar respectively
-	printf("%d\n", a);
+	printf("%d\n", spam::a);
 	printf("%f\n", b);
+
+	//using namespace
+	{
+		using namespace foo;
+		cout << a << endl;
+	}
+	{
+		using namespace bar;
+		cout << a << endl;	//apparently a is still from foo in this block because of the earlier using
+	}
+
+	int wtf;	//so apparently this auto initializes to 0
+	cout << wtf << endl;
 
 	return 0;
 }
