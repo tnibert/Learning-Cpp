d02fa441f5c0b28e1c7f0efe5d252f411c0ed3a5
Tim <docz2a@gmail.com>
2017-03-31 08:37:43 +1100

    more play

--- arrays.cpp
+++ arrays.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//C++ passes arrays as pointers
+//when you pass a single indexed array, you can declare arr[]
+//additional indexes require a value in the declaration
+void func (int arr[][2][3])
+{
+	for (int i : arr)	//well, this doesn't work...
+	{
+		cout << i;
+	}
+}
+
 int main()
 {
+
+	int arr [3][2][3] = {1, 2, 3,4,5,6,7,8};
+
+	//func(arr);
+
 	int foo[5] = { 12,  20, 3, 4, 10 };
 
 	for (int i : foo)
@@ -13,6 +29,20 @@ int main()
 	for (int i = 0; i < 5; i++)
 	{
 		cout << foo[i] << endl;
+	}
+
+	int bar[5] = {10,30}; 		//initialize first 2 vals, rest are zeros
+
+	for (int i : bar)
+	{
+		printf("%d\n", i);
+	}
+
+	int spam[] { 1, 2, 3, 4 };	//no = sign needed, auto sets to size 4
+
+	for (int i : spam)
+	{
+		cout << i << endl;
 	}
 
 	return 0;
