9432b93cbb448b0b354180ce528ae9bb093459ed
Tim <docz2a@gmail.com>
2017-03-31 09:18:21 +1100

    finished strings and char arrays

--- strings.cpp
+++ strings.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main ()
@@ -16,6 +17,27 @@ int main ()
 	bar[1] = 'a';
 	bar[2] = 'r';
 	cout << bar;
+
+	//the above are C strings, must differentiate between that and library strings
+
+	char question1[] = "What is your name? ";
+	string question2 = "Where do you live? ";
+	char answer1[20];	//apparently if we overflow this it will print but then seg fault, or stack smash detected with core dump
+	string answer2;
+	cout << question1;
+	cin >> answer1;
+	cout << question2;
+	cin >> answer2;
+
+	cout << "Hello, " << answer1;
+	cout << " from " << answer2 << "!\n";
+
+	//converting between C strings and lib strings
+	char cstr[] = "text";
+	string libstr = cstr;
+	cout << libstr;
+	cout << libstr.c_str();
+	cout << endl;
 
 	return 0;
 }
