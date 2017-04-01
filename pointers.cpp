f91f9370242979712269b837f7b09139ae3395c3
Tim <docz2a@gmail.com>
2017-04-01 13:06:05 +1100

    array pointers!

--- pointers.cpp
+++ pointers.cpp
@@ -17,7 +17,7 @@ int main ()
 
 	//ok lets do a demo
 	int val1, val2;
-	int * ptr;
+	int * ptr, * ptr2;
 
 	//you can assign a pointer to an uninitialized variable
 	ptr = &val1;
@@ -25,6 +25,44 @@ int main ()
 	ptr = &val2;	//we can change the variable we point to
 	*ptr = 20;
 	printf("val1 is %d, val2 is %d\n", val1, val2);
+
+	ptr2 = ptr;	//both now reference val2
+	*ptr = 25;
+	cout << *ptr2 << endl;	//now we print 25
+
+	/* arrays work like pointers that point to their first value */
+	int arr[9] = {1,2,3,4,5,6,7,8,9};
+	int * arrptr = arr;
+	cout << arr << endl << arrptr << endl;
+	cout << arrptr[3] << endl;
+
+	/*whaaaat
+	int main ()
+	{
+	  int numbers[5];
+	  int * p;
+	  p = numbers;  *p = 10;
+	  p++;  *p = 20;
+	  p = &numbers[2];  *p = 30;
+	  p = numbers + 3;  *p = 40;
+	  p = numbers;  *(p+4) = 50;
+	  for (int n=0; n<5; n++)
+	    cout << numbers[n] << ", ";
+	  return 0;
+	}
+	*/
+	*arrptr = -1;
+	arrptr++;	//address of next element?
+	*arrptr = -2;
+	arrptr = &arr[4];
+	*arrptr = -5;
+	arrptr += 3;
+	*arrptr = -8;
+	for (int i : arr)
+	{
+		cout << i;
+	}
+	cout << endl;
 
 	return 0;
 }
