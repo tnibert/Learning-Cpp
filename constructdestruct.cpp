b387b232efbfb7dc3b5e51f21e5ec738168259c9
Tim <docz2a@gmail.com>
2017-04-13 08:21:00 +1000

    copy assignment

--- constructdestruct.cpp
+++ constructdestruct.cpp
@@ -7,8 +7,8 @@ class Example4 {
     string* ptr;
   public:
     // constructors:
-    Example4() : ptr(new string) {}
-    Example4 (const string& str) : ptr(new string(str)) {}
+    Example4() : ptr(new string) { printf("In default constructor\n"); }
+    Example4 (const string& str) : ptr(new string(str)) { printf("In constructor with arg\n"); }
     // destructor:
     ~Example4 () {
 	delete ptr;
@@ -19,9 +19,22 @@ class Example4 {
 
     // copy constructor to deep copy
     // shallow copy means copying each variable value, e.g. pointer addresses so multiple objects would point to same memory
+    // this is just when you construct an object, not when you assign it
     Example4 (const Example4& x) : ptr(new string(x.content())) {
 	printf("In copy constructor\n");
 	}
+    // for assignment you need to overload assignment operator
+    // implicit operator does shallow copy
+    // explicit:
+    Example4& operator= (const Example4& x) {
+	printf("In copy assignment\n");
+	*ptr = x.content();
+	return *this;
+    }
+
+    void printPtrAddr(){
+	cout << ptr << endl;
+    }
 };
 
 int main () {
@@ -30,7 +43,14 @@ int main () {
 
   cout << "bar's content: " << bar.content() << '\n';
 
-  //I think destructor is automatically called at end of program
+  foo = bar;
+
+  foo.printPtrAddr();
+  bar.printPtrAddr();
+
+  cout << "foo's content: " << foo.content() << "\n";
+
+  //destructor is automatically called at end of program
 
   return 0;
 }
